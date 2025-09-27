e7725c6f138532aeda36d1fe38fc69513e699a86
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-28 00:07:08 +0800

    wip: UI for the landing page + logic when uploading file

---
+++ app/(home)/homeLandingPage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeLandingPage = () => {
+  return (
+    <View>
+      <Text>HomeLandingPage</Text>
+    </View>
+  )
+}
+
+export default HomeLandingPage
