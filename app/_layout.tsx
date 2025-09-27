7a6fa59a5c8956ae16bd886c631144f6acf7213e
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-28 00:05:16 +0800

    feat: Edited the root layout for multiple pages + handling of preferred theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,82 @@
+
+import AlertPersonalization from '@/components/AlertPersonalization';
 import '@/global.css';
 
 import { NAV_THEME } from '@/lib/theme';
+import { checkIfKeyExists } from '@/util/checkIfNewUser';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
-import { Stack } from 'expo-router';
+import { useNavigationContainerRef } from 'expo-router';
+import Drawer from 'expo-router/drawer';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
+import { useEffect, useState } from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
+  const { setColorScheme, colorScheme } = useColorScheme();
+  const [ready, setReady] = useState(false);
+  const navigationRef = useNavigationContainerRef();
+
+  const [openPersonalizationAlert, setOpenPersonalizationAlert] = useState<boolean>(false);
+
+  useEffect(() => {
+    const unsubscribe = navigationRef.addListener('state', async () => {
+      const exists = await checkIfKeyExists('name');
+      setOpenPersonalizationAlert(!exists);
+    });
+
+    return unsubscribe;
+  }, [navigationRef]);
+
+  useEffect(() => {
+    const initTheme = async () => {
+      try {
+        const storedTheme = await AsyncStorage.getItem("theme");
+        if (storedTheme === "light" || storedTheme === "dark") {
+          setColorScheme(storedTheme);
+        } else {
+          setColorScheme("system");
+        }
+      } catch (err) {
+        console.error("Error loading theme:", err);
+        setColorScheme("system");
+      } finally {
+        setReady(true);
+      }
+    };
+
+    initTheme();
+  }, []);
 
   return (
     <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
+      <AlertPersonalization open={openPersonalizationAlert} onOpenChange={setOpenPersonalizationAlert} />
+      <Drawer screenOptions={{
+        headerTintColor: "#3B82F6",
+        drawerActiveTintColor: "#3B82F6",
+        drawerInactiveTintColor: "#9CA3AF",
+        drawerActiveBackgroundColor: "#E0F2FE",
+      }}>
+        <Drawer.Screen name="(home)" options={{
+          headerShown: false, drawerLabel: "Home",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }} />
+        <Drawer.Screen name="(settings)" options={{
+          headerShown: false, drawerLabel: "Settings", title: "Settings",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }} />
+      </Drawer>
       <PortalHost />
     </ThemeProvider>
   );
