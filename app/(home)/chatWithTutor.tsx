24e6c57da62ed29121ff138b77b48a49ffa8fb32
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-29 18:36:32 +0800

    feat: Installed lucide react native

--- app/(home)/chatWithTutor.tsx
+++ app/(home)/chatWithTutor.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TextInput, Platform, KeyboardAvoidingView } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Content, getAIResponse } from '@/util/conversationalAI';
@@ -43,15 +43,22 @@ const ChatWithTutor = () => {
 
 
   return (
-    <SafeAreaView className="flex-1 justify-start items-start bg-background px-6 py-4" edges={["left", "right", "bottom"]}>
-      <ScrollView className="w-full" showsVerticalScrollIndicator={false}>
-        {conversation.length === 0 && (
-          <View className="flex-1 justify-center items-center">
+    <SafeAreaView className="flex-1 justify-start items-start bg-background px-6 py-4 gap-2" edges={["left", "right", "bottom"]}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={0}
+      >
+        <ScrollView className="min-w-full flex-1" contentContainerStyle={{ flexGrow: 1 }}>
+          {conversation.length === 0 && (
+            <View className="flex-1 justify-center items-center">
+              {/* Your empty state content */}
+            </View>
 
-          </View>
-        )}
-      </ScrollView>
-      <Input className='w-full'/>
+          )}
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <Input className='w-full border-gray-400' />
     </SafeAreaView>
   )
 }
