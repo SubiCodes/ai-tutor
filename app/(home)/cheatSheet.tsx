27345bcaa01ebf6619f3879b3e8a64ad6dc7363a
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-29 23:12:04 +0800

    fix: Multiple runs of PRAGMA = WAL causing DB errors

--- app/(home)/cheatSheet.tsx
+++ app/(home)/cheatSheet.tsx
@@ -1,19 +1,36 @@
 import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { createStudyNotes } from '@/util/createNotes'
 import { getDb } from '@/db/db'
 import * as SQLite from "expo-sqlite";
 import Markdown from 'react-native-markdown-display'
+import { useFocusEffect } from 'expo-router'
+import { getCheatSheet, postToCheatSheet } from '@/db/cheatSheetFunctions'
 
 const CheatSheet = () => {
     const [db, setDb] = useState<SQLite.SQLiteDatabase | null>(null);
     const [lecture, setLecture] = useState<string>('');
 
+    const fetchCheatSheet = async () => {
+        if (!db) return;
+
+        const allRows = await getCheatSheet(db);
+        console.log("Cheat Sheet:", allRows);
+
+        if (allRows.length > 0) {
+            setLecture(allRows[0].lecture);
+        } else {
+            setLecture('');
+        }
+    };
+
+
     const generateCheatSheet = async () => {
         if (!db) return;
         const lecture = await createStudyNotes(db);
-        setLecture(lecture)
+        setLecture(lecture);
+        await postToCheatSheet(db, lecture)
     }
 
     useEffect(() => {
@@ -22,6 +39,15 @@ const CheatSheet = () => {
             setDb(database);
         })();
     }, []);
+
+    useFocusEffect(
+        useCallback(() => {
+            if (db) {
+                fetchCheatSheet();
+            }
+        }, [db])
+    );
+
     return (
         <View>
             <Button onPress={() => generateCheatSheet()}>
--- db/db.ts
+++ db/db.ts
@@ -5,24 +5,26 @@ let db: SQLite.SQLiteDatabase | null = null;
 export const getDb = async (): Promise<SQLite.SQLiteDatabase> => {
   if (!db) {
     db = await SQLite.openDatabaseAsync("aiTutor");
+
+    await db.execAsync(`PRAGMA journal_mode = WAL;`);
+
     await db.execAsync(`
-      PRAGMA journal_mode = WAL;
       CREATE TABLE IF NOT EXISTS embeddings (
         id INTEGER PRIMARY KEY NOT NULL,
         text TEXT NOT NULL,
         embedding BLOB NOT NULL
       );
     `);
+
     await db.execAsync(`
-      PRAGMA journal_mode = WAL;
       CREATE TABLE IF NOT EXISTS conversation (
         id INTEGER PRIMARY KEY NOT NULL,
         role TEXT NOT NULL,
         message TEXT NOT NULL
       );
     `);
+
     await db.execAsync(`
-      PRAGMA journal_mode = WAL;
       CREATE TABLE IF NOT EXISTS cheatSheet (
         id INTEGER PRIMARY KEY NOT NULL,
         lecture TEXT NOT NULL
