a3c9e312c8d80efe8d2fa92e594530d6f7949cce
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-02 18:14:16 +0800

    feat: Created the functions for posting deleting and fetching the flashcards

---
+++ db/flashCardFunctions.ts
@@ -0,0 +1,19 @@
+import * as SQLite from "expo-sqlite";
+
+export const postToFlashCard = async (
+    db: SQLite.SQLiteDatabase, questions: string
+    ) => {
+    const result = await db.runAsync(
+        "INSERT INTO flashCards (questions) VALUES (?)",
+        [questions]
+    );
+};
+
+export const getFlashCard = async (db: SQLite.SQLiteDatabase) => {
+    const allRows = await db.getAllAsync('SELECT * FROM flashCards');
+    return allRows as { id: number; lecture: string }[];
+}
+
+export const deleteFlashCardTableData = async (db: SQLite.SQLiteDatabase) => {
+    await db.execAsync("DELETE FROM flashCards;");
+};
