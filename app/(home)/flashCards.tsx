e3141ce64c5898ac7b1584b32db47a2507cf6442
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-02 18:24:53 +0800

    feat: Working saving deleting and fetching of uploaded flash cards

--- app/(home)/flashCards.tsx
+++ app/(home)/flashCards.tsx
@@ -5,17 +5,31 @@ import { useFocusEffect } from 'expo-router'
 import { createFlashCardsString, parseQuestionsToJson } from '@/util/createFlashCards'
 import { getDb } from '@/db/db'
 import * as SQLite from "expo-sqlite";
+import { deleteFlashCardTableData, getFlashCard, postToFlashCard } from '@/db/flashCardFunctions'
+
+export type FlashCard = {
+  question: string;
+  answer: string;
+};
+
+export type FlashCards = FlashCard[];
 
 const FlashCards = () => {
 
     const [db, setDb] = useState<SQLite.SQLiteDatabase | null>(null);
+    const [flashCards, setFlashCards] = useState<FlashCards>();
 
     const generateFlashcards = async () => {
-        if(!db) return
+        if (!db) return
         const rawQuestions = await createFlashCardsString(db);
         //console.log("Raw Questions: ",rawQuestions);
+
+        await deleteFlashCardTableData(db);
+        await postToFlashCard(db, rawQuestions);
+
         const parsedQuestions = await parseQuestionsToJson(rawQuestions);
         //console.log("Parsed Questions: ",parsedQuestions);
+        setFlashCards(parsedQuestions);
     }
 
     useEffect(() => {
