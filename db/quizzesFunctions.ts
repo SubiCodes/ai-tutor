63f349544a9be34b40c66a0c6406ffd70cf890ef
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-06 20:39:42 +0800

    feat: Created the function for getting the specific quiz result.

--- db/quizzesFunctions.ts
+++ db/quizzesFunctions.ts
@@ -1,16 +1,23 @@
 import * as SQLite from "expo-sqlite";
 
-export const postToQuizzes = async ( db: SQLite.SQLiteDatabase, quiz: string, score: number, total: number, type: string ) => {
+export const postToQuizzes = async (db: SQLite.SQLiteDatabase, quiz: string, score: number, total: number, type: string) => {
     const result = await db.runAsync(
         "INSERT INTO quizzes (quiz, score, total, type) VALUES (?, ?, ?, ?)",
         [quiz, score, total, type]
     );
 };
 
-export const getFlashCard = async (db: SQLite.SQLiteDatabase) => {
-    const allRows = await db.getAllAsync('SELECT * FROM flashCards');
-    return allRows as { id: number; questions: string }[];
-}
+export const getQuizResult = async (db: SQLite.SQLiteDatabase, id: number) => {
+    const allRows = await db.getAllAsync("SELECT * FROM quizzes WHERE id = ?", [id]);
+    return allRows as {
+        id: number;
+        quiz: string;
+        score: number;
+        total: number;
+        type: string;
+        date: string;
+    }[];
+};
 
 export const deleteFlashCardTableData = async (db: SQLite.SQLiteDatabase) => {
     await db.execAsync("DELETE FROM flashCards;");
