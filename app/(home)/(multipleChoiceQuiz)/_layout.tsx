80ce62f7c91f27c8b7c8aeeb0ae5f51e0fab06c3
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-06 21:01:46 +0800

    feat: Created the pages for multiple choices quiz

---
+++ app/(home)/(multipleChoiceQuiz)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { NAV_THEME } from '@/lib/theme';
+import { ThemeProvider, DrawerActions, useNavigation } from '@react-navigation/native';
+import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
+import DrawerToggle from '@/components/DrawerToggle';
+
+export default function AuthLayout() {
+  const { colorScheme } = useColorScheme();
+  return (
+    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: 'slide_from_right',
+          headerTintColor: '#3B82F6',
+        }}
+      >
+        <Stack.Screen
+          name="homeLandingPage"
+          options={{
+            title: 'main',
+            headerShown: true,
+            headerLeft: () => <DrawerToggle />,
+          }}
+        />
+        <Stack.Screen
+          name="quiz"
+          options={{
+            title: 'Goodluck!!!',
+            headerShown: true,
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
+  );
+}
