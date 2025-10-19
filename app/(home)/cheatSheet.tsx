2d19ec058a49ba8857f74da4e26bc3f063575cbc
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-19 23:17:45 +0800

    feat: Added a generate new cheatsheet on cheatsheet page

--- app/(home)/cheatSheet.tsx
+++ app/(home)/cheatSheet.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native'
 import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { createStudyNotesChunked } from '@/util/createNotes'
@@ -10,6 +10,7 @@ import { deleteCheatSheetTableData, getCheatSheet, postToCheatSheet } from '@/db
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Sparkles } from 'lucide-react-native'
 import { deleteEmbeddingsTableData } from '@/db/embeddedChunksFunctions'
+import Toast, { Toast as ToastFunc } from 'toastify-react-native'
 
 const CheatSheet = () => {
     const [db, setDb] = useState<SQLite.SQLiteDatabase | null>(null);
@@ -31,14 +32,37 @@ const CheatSheet = () => {
 
     const generateCheatSheet = async () => {
         setGeneratingLecture(true);
+        ToastFunc.show({
+            type: 'info',
+            text1: 'Generating CheatSheet',
+            text2: `This might take a while.`,
+            position: 'bottom',
+            visibilityTime: 4000,
+            autoHide: true,
+        })
         try {
             if (!db) return;
             await deleteCheatSheetTableData(db)
             const lecture = await createStudyNotesChunked(db);
             setLecture(lecture);
-            await postToCheatSheet(db, lecture)
+            await postToCheatSheet(db, lecture);
+            ToastFunc.show({
+                type: 'success',
+                text1: 'CheatSheet successfully generated',
+                text2: `Goodluck with your studies!!!`,
+                position: 'bottom',
+                visibilityTime: 4000,
+                autoHide: true,
+            })
         } catch (error) {
-
+            ToastFunc.show({
+                type: 'error',
+                text1: 'Unable to generate CheatSheet',
+                text2: `Please try again.`,
+                position: 'bottom',
+                visibilityTime: 4000,
+                autoHide: true,
+            })
         } finally {
             setGeneratingLecture(false);
         }
@@ -66,32 +90,46 @@ const CheatSheet = () => {
     );
 
     return (
-        <SafeAreaView className="flex-1 justify-start items-start bg-background px-4 pb-4 pt-0 gap-2" edges={["left", "right", "bottom"]}>
+        <SafeAreaView className="flex-1 bg-background px-4 pb-4 pt-0" edges={["left", "right", "bottom"]}>
             {!lecture ? (
-                <View className='w-full h-full items-center justify-center gap-4'>
+                <View className='flex-1 w-full items-center justify-center gap-4'>
                     <Text className='text-2xl text-blue-500 font-bold'>No CheatSheet Yet</Text>
                     <TouchableOpacity
                         className="flex-row items-center justify-center p-4 bg-gray-50 rounded-2xl border border-gray-100 w-[80%]"
-                        onPress={() => generateCheatSheet()} disabled={generatingLecture}
+                        onPress={() => generateCheatSheet()}
+                        disabled={generatingLecture}
                     >
                         <View className="w-10 h-10 rounded-full bg-white items-center justify-center mr-3 shadow-sm">
                             <Sparkles color={'#3B82F6'} size={24} />
                         </View>
-                        <Text className="text-base text-gray-700">
-                            Generate Lecture
-                        </Text>
+                        <Text className="text-base text-gray-700">Generate Lecture</Text>
                     </TouchableOpacity>
                 </View>
             ) : (
-                <ScrollView
-                    className="flex-1 w-full min-h-full"
-                    contentContainerStyle={{ paddingBottom: 20, paddingTop: 20, paddingHorizontal: 16 }}
-                >
-                    <Markdown>{lecture}</Markdown>
-                </ScrollView>
+                <View className="flex-1 w-full">
+                    <ScrollView
+                        className="flex-1 w-full"
+                        contentContainerStyle={{ paddingBottom: 100, paddingHorizontal: 16 }}
+                    >
+                        <Markdown>{lecture}</Markdown>
+                    </ScrollView>
+
+                    <View className='absolute bottom-4 left-4 right-4'>
+                        <TouchableOpacity
+                            onPress={() => generateCheatSheet()}
+                            className='bg-blue-500 rounded-xl p-4 items-center justify-center flex-row gap-4 '
+                            activeOpacity={.7}
+                            disabled={generatingLecture}
+                        >
+                            {generatingLecture && (
+                                <ActivityIndicator size="small" color="#fff" />
+                            )}
+                            <Text className='text-white font-medium'>Generate New Lecture</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             )}
-
-
+            <Toast/>
         </SafeAreaView>
     )
 }
